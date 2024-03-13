28c8bc6757b743972afccb50cb25b2ec19f580e0
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-13 14:11:23 +0100

    Add Landing Page

---
+++ issuer/frontend/src/routes/(landing-page)/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<div class="relative isolate px-6 pt-14 lg:px-8">
+    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 flex justify-center items-center flex-col gap-8">
+      <div class="text-center">
+        <h1 class="text-4xl font-bold tracking-tight sm:text-6xl">Welcome to VC Playground</h1>
+      </div>
+			<button type="button" class="btn variant-filled">Connect With Internet Identity</button>
+    </div>
+  </div>
+</AppShell>
