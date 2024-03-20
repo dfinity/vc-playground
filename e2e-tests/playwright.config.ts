8f74461d5047e0dc83eefe763adff5609e772185
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-20 14:10:39 +0100

    Add failure report

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -1,6 +1,17 @@
 import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+  testDir: 'src',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.PLAYWRIGHT_BASE_URL ?? 'http://localhost:5173/',
@@ -11,7 +22,6 @@ const config: PlaywrightTestConfig = {
     // headless: false,
     testIdAttribute: 'data-tid',
   },
-  testDir: 'src',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
   /* Configure projects for major browsers */
   projects: [
