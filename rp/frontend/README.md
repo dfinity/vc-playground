74f0940eb7b48dd674035ea961e61e9b9cc4ee70
Llorenç <llorenc.muntaner@dfinity.org>
2024-03-21 15:03:54 +0100

    Setup RP frontend

---
+++ rp/frontend/README.md
@@ -0,0 +1,3 @@
+# Relying Party Frontend
+
+TBD
---
+++ rp/frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import MainWrapper from '$lib/components/MainWrapper.svelte';
+import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<MainWrapper>
+		<!-- Page Route Content -->
+		<slot />
+	</MainWrapper>
+</AppShell>
