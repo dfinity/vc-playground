c9492e861afa7e33126a0e0455c6336d107ff77a
Llorenç <llorenc.muntaner@dfinity.org>
2024-04-04 12:53:38 +0200

    Choose issuer when sharing image

--- rp/frontend/src/lib/stores/issuers.store.ts
+++ rp/frontend/src/lib/stores/issuers.store.ts
@@ -1,21 +1,28 @@
-import { writable, type Writable } from 'svelte/store';
-import { AnonymousIdentity, type Identity } from '@dfinity/agent';
+import { derived, writable, type Readable } from 'svelte/store';
+import { AnonymousIdentity } from '@dfinity/agent';
 import { queryGroups } from '$lib/api/queryGroups.api';
 import { browser } from '$app/environment';
 import type { PublicGroupData } from '../../declarations/meta_issuer/meta_issuer.did';
+import type { Principal } from '@dfinity/principal';
 
-let issuersStore: Writable<PublicGroupData[] | undefined> | undefined = undefined;
-export const getIssuersStore = (
-  identity: Identity | undefined | null
-): Writable<PublicGroupData[] | undefined> => {
-  if (!issuersStore) {
-    issuersStore = writable<PublicGroupData[] | undefined>(undefined, (_set, update) => {
-      if (browser) {
-        queryGroups({ identity: identity ?? new AnonymousIdentity() }).then((groups) => {
-          update(() => groups);
-        });
-      }
+const groupsStore = writable<PublicGroupData[] | undefined>(undefined, (_set, update) => {
+  if (browser) {
+    queryGroups({ identity: new AnonymousIdentity() }).then((groups) => {
+      update(() => groups);
     });
   }
-  return issuersStore;
-};
+});
+
+export const credentialsTypesStore: Readable<string[]> = derived(groupsStore, (issuers) =>
+  Array.from(new Set(issuers?.map((issuer) => issuer.group_name)).values())
+);
+
+export type Issuer = { nickname: string; owner: Principal };
+export const issuersStore: Readable<Issuer[]> = derived(groupsStore, (issuers) =>
+  Array.from(new Set(issuers?.map((issuer) => issuer.issuer_nickname)).values()).map(
+    (nickname) => ({
+      nickname,
+      owner: issuers?.find((issuer) => issuer.issuer_nickname === nickname)?.owner as Principal,
+    })
+  )
+);
