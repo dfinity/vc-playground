5f7e4e4d41bda311df2213e60fd4b42aec98c410
Llorenç <llorenc.muntaner@dfinity.org>
2024-04-10 19:08:49 +0200

    Improve HeroCard

--- rp/frontend/src/lib/components/HeroCard.svelte
+++ rp/frontend/src/lib/components/HeroCard.svelte
@@ -8,7 +8,7 @@
   data-tid={testId}
   class="card card-hover p-4 flex flex-col justify-center items-center"
 >
-  <div class="p-8 flex justify-center items-center">
+  <div class="sm:p-8 p-4 flex justify-center items-center">
     <slot name="icon" />
   </div>
   <h3 class="h3 text-center"><slot name="title" /></h3>
