62e9fb404c698650c863b0b6efd0bbfe09c40a04
Llorenç <llorenc.muntaner@dfinity.org>
2024-04-12 15:58:56 +0200

    Deploy RP with script

--- scripts/deploy-meta-issuer.sh
+++ scripts/deploy-meta-issuer.sh
@@ -117,4 +117,6 @@ echo "Using II canister: $II_CANISTER_ID" >&2
 # Adjust issuer's .well-known/ii-alternative-origins to contain FE-hostname of local/dev deployments.
 sed -i "s+ISSUER_FE_HOSTNAME_PLACEHOLDER+\"$ISSUER_FRONTEND_HOSTNAME\",+" ./issuer/frontend/static/.well-known/ii-alternative-origins
 
+cat ./issuer/frontend/static/.well-known/ii-alternative-origins
+
 dfx deploy meta_issuer --network "$DFX_NETWORK" --argument '(opt record { idp_canister_ids = vec{ principal "'"$II_CANISTER_ID"'" }; ic_root_key_der = vec '"$rootkey_did"'; derivation_origin = "'"$ISSUER_DERIVATION_ORIGIN"'"; frontend_hostname = "'"$ISSUER_FRONTEND_HOSTNAME"'"; })'
