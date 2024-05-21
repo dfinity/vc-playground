b2504fe9e21f4bbae99ba07738af71214ec98579
Llorenç <llorenc.muntaner@dfinity.org>
2024-05-21 09:22:44 +0200

    Fix inputTypeCredentialSpec

--- issuer/frontend/src/lib/utils/input-type-credential-spec.utils.ts
+++ issuer/frontend/src/lib/utils/input-type-credential-spec.utils.ts
@@ -1,9 +1,13 @@
 import type { CredentialSpec } from '../../declarations/meta_issuer.did';
 
-export const inputTypeCredentialSpec = (spec: CredentialSpec): 'text' | 'number' => {
-  if (spec.arguments.length > 1 || spec.arguments.length === 0) {
+export const inputTypeCredentialSpec = (spec: CredentialSpec): 'text' | 'number' | undefined => {
+  const specArguments = spec.arguments[0];
+  if (specArguments === undefined) {
+    return undefined;
+  }
+  if (specArguments.length > 1) {
     throw new Error('Only one argument is supported');
   }
-  const argument = spec.arguments[0];
+  const [_, argument] = specArguments[0];
   return 'Int' in argument ? 'number' : 'text';
 };
