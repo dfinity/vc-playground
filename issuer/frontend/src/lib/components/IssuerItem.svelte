26295dffe15a983ae6703a461d912a74295faea4
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2024-06-17 11:00:54 +0200

    Display badges with approved and pending count
    On the issuer control center overview badges are added that show the
    number of pending and approved credentials.

--- issuer/frontend/src/lib/components/IssuerItem.svelte
+++ issuer/frontend/src/lib/components/IssuerItem.svelte
@@ -2,6 +2,10 @@
   import ListItem from '$lib/ui-components/elements/ListItem.svelte';
   import { vcArgumentsValue } from '$lib/utils/vc-arguments-value.utils';
   import type { PublicGroupData } from '../../declarations/meta_issuer.did';
+  import Badge from "$lib/ui-components/elements/Badge.svelte";
+  import {countApprovedCredentials, countPendingCredentials} from "$lib/utils/count-approved-credentials.utils";
+  import {getIssuerDetailStore, type IssuerDetailStore} from "$lib/stores/issuer-detail.store";
+  import {authStore} from "$lib/stores/auth.store";
 
   export let issuer: PublicGroupData;
   export let onClick: (() => void) | undefined = undefined;
@@ -11,6 +15,16 @@
   // This might happen because admins can request credentials to themselves.
   let vcArgumentValue: string | number | undefined;
   $: vcArgumentValue = isAdmin ? undefined : vcArgumentsValue(issuer.vc_arguments);
+
+  let issuerStore: IssuerDetailStore;
+  $: issuerStore = getIssuerDetailStore({
+    identity: $authStore.identity,
+    issuerName: issuer.group_name ?? '',
+  });
+  let approvedCredentials: number;
+  $: approvedCredentials = countApprovedCredentials($issuerStore?.members ?? []);
+  let pendingCredentials: number;
+  $: pendingCredentials = countPendingCredentials($issuerStore?.members ?? []);
 </script>
 
 <ListItem testId={`credentials ${issuer.issuer_nickname} ${issuer.group_name}`} {onClick}>
@@ -19,6 +33,10 @@
     {#if vcArgumentValue !== undefined}
       {` - ${vcArgumentValue}`}
     {/if}
+    <p>
+      <Badge variant="success">Approved: {approvedCredentials}</Badge>
+      <Badge variant="default">Pending: {pendingCredentials}</Badge>
+    </p>
   </svelte:fragment>
   <span slot="sub">{`Issued by ${issuer.issuer_nickname}`}</span>
   <slot name="end" slot="end" />
